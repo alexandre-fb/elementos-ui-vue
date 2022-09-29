d1069c0a9ccc874bfa660101bc898ec420e031c8
Alexandre Bisognin <alexandrefb.86@gmail.com>
2022-09-29 15:06:51 -0300

    Initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Elementos UI criados com Vue.js</h1>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <TabComponent/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TabComponent from '@/components/TabComponent.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    TabComponent
+  }
+}
+</script>
